4649e28b6c228dc045da00c2b6ddf5318139b132
dev123 <hyunho3445@gmail.com>
2025-05-03 00:13:29 +0900

    ver2. map - company marker / radius

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    budget = request.args.get("budget", default=100000, type=int)
+    area = request.args.get("area", default=60, type=float)
+    work_address = request.args.get("work_address", default="서울특별시 중구 세종대로 110", type=str)
+    use_radius = 'use_radius' in request.args
+    return render_template("index.html", budget=budget, area=area, work_address=work_address, use_radius=use_radius)
+
+
 def map():
     return render_template("index.html")
 
